6375126ce8b9b273c89fe8a8fe69d68dbc3bdfa5
ZainUlAbideen444 <mrzeeofficial.4@gmail.com>
2025-08-15 18:23:39 +0500

    if else padhaya mene

---
+++ mama.js
@@ -0,0 +1,10 @@
+let weather = "cold";
+
+if(weather === "sunny") {
+    console.log("nikal mere garmi k kappry or sunglasses");
+}
+else if(weather === "rainy") {
+    console.log("nikal meri chhatri or raincoat");
+} else{
+    console.log("nikal mere thande mausam k sweater or jacket");
+}
