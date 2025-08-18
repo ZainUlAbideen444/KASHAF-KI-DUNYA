909aba50e7e435a55a83e4acf8d04a07d265c849
ZainUlAbideen444 <mrzeeofficial.4@gmail.com>
2025-08-18 18:58:22 +0500

    function b horhe hain

--- mama.js
+++ mama.js
@@ -1,6 +1,37 @@
-for(let i =1; i <= 50; i++){
-    if(i % 3===0){
-        console.log(i);
-        
-    }
+// for(let i=1; i <= 20; i++){
+//   if(i %2===1){
+//     console.log(`odd number is ${i}`);
+    
+//   }
+// }
+
+// function are block of code to perform a specific task
+// reusable 
+// numbers, keywords, special characters, operators,
+//  hum jo bhi code likhty wo foran run hojata hai, lekin function tab run hota hai jab hum usay call karte hain
+
+// function add(v1,v2){
+//   console.log(v1+v2);
+  
+// }
+// add(5,5);
+
+// rest operators
+// function gulab(...val){
+//   console.log(val);
+  
+// }
+// gulab(1,2,3,4,5,6,7,8,9,10);
+
+// add ();
+
+// let fnc = function add() {
+//   console.log("Hello, I am a function");
+  
+// }
+function greet(val){
+  return 0;
 }
+ 
+let salam = greet(5);
+console.log(salam);
